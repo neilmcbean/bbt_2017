3ac197a25a2b4e2f1b7d8a7df1051d54e8e8994f
AURORA-IVO\Ivo van der Marel <ivo.marel@gmail.com>
2017-05-23 13:08:11 -0700

    Updated TransformMoveEvent Editor to have PreviewCam button

---
+++ BBT_2017_UP/Assets/Scripts/ObjEvents/Editor/TransformMoveEventEditor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(TransformMoveEvent), true)]
+public class TransformMoveEventEditor : TweenEventEditor
+{
+    Camera targetCam;
+    Camera previewCam;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        TransformMoveEvent cast = (TransformMoveEvent)target;
+        cast.tweenDuration = EditorGUILayout.FloatField("Duration", cast.tweenDuration);
+        cast.targetObj = (Transform)EditorGUILayout.ObjectField("Target", cast.targetObj, typeof(Transform), true);
+
+        targetCam = cast.targetObj.GetComponent<Camera>();
+        previewCam = cast.GetComponent<Camera>();
+
+        if (previewCam != null)
+        {
+            if (GUILayout.Button("Disable Cam Preview"))
+            {
+                DestroyImmediate(previewCam);
+            }
+        }
+
+        if (targetCam != null)
+        {
+            if (previewCam == null)
+            {
+                if (GUILayout.Button("Enable Cam Preview"))
+                {
+                    previewCam = cast.gameObject.AddComponent<Camera>();
+                    ComponentUtility.MoveComponentUp(previewCam);
+                    UpdateCamFromMain();
+                }
+            }
+        }
+    }
+
+    void UpdateCamFromMain ()
+    {
+        if (targetCam != null)
+        {
+            if (ComponentUtility.CopyComponent(targetCam))
+            {
+                ComponentUtility.PasteComponentValues(previewCam);
+            }
+        } 
+    }
+}
