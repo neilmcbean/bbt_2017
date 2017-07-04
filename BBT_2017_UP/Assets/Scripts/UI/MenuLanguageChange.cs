4169315f2ccf201866b25a1a525ff4df70d21be8
Maurice Grela <maurice.grela@gmail.com>
2017-07-04 16:57:29 -0700

    adding the first draft of the functional UI which allows players to change languages.

---
+++ BBT_2017_UP/Assets/Scripts/UI/MenuLanguageChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuLanguageChange : MonoBehaviour {
+
+    public GameObject PageManagerRef;
+    public Dropdown dropdownMenu;
+
+    // Use this for initialization
+    void Start() {
+        
+        dropdownMenu = GetComponent<Dropdown>();
+        for (int languageCount = 0; languageCount < DataManager.languageManager.Length; languageCount++)
+        {
+            dropdownMenu.options.Add(new Dropdown.OptionData() { text = DataManager.languageManager[languageCount] });
+            //dropdownMenu.options[languageCount].text = "test";
+            // dropdownMenu.value = languageCount;
+            //dropdownMenu.value = languageCount;
+            //dropdownMenu.options.Add(new Dropdown.OptionData(dropdownMenu.options[languageCount].text));
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void LanguageUpdate()
+    {
+        PageManagerRef.GetComponent<PageManager>().ChangeLanguage(GetComponentInChildren<Text>().text);
+        //Debug.Log(GetComponentInChildren<Text>().text);
+    }
+}
