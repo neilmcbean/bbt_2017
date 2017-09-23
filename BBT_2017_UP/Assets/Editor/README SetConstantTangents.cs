370e82e751a2f544a1ccc1d4b61bdb7fd65f5e84
Diego De la Rocha <hi@diegodelarocha.com>
2017-09-23 11:43:39 -0700

    Instructions for how to use SetConstantTangents.cs

---
+++ BBT_2017_UP/Assets/Editor/README SetConstantTangents.cs
@@ -0,0 +1,13 @@
+﻿// SetConstantTangents works the following way:
+
+// Add SetConstantTangents.cs to the editor
+// Impport Animation on prefab
+// Resample curves = False
+// Anim. Compression = Off
+// Hit Apply
+// Select all Animation Clips on the Project's Prefab
+// Duplicate (Command + D)
+// Add the new duplicates on a folder (to keep organized)
+// Select them all and go to Tools > SelectConstantTangents
+
+// Once the operation is done, the selected Animation scripts should have Constant Tangents
