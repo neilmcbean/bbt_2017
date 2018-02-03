ea211edcfe4ee90d7d981ebb8f954c3e1a9e176a
Maurice Grela <maurice.grela@gmail.com>
2018-02-02 17:37:10 -0800

    removing a level adding implemented definitions

--- BBT_2017_UP/Assets/Scripts/UI/DefinitionRenderer.cs
+++ BBT_2017_UP/Assets/Scripts/UI/DefinitionRenderer.cs
@@ -5,14 +5,21 @@ using UnityEngine.UI;
 
 public class DefinitionRenderer : MonoBehaviour {
 
+	public Text title;
+	public Text TextTranslation;
+	public Text TextBody;
+
+
 	public Image RenderPlacement;
 
 	public string[] def_wordSas;
+	public string[] def_wordSasTranslation;
 	public Sprite [] def_wordSasPhotos;
 	public string[] def_BodySasEnglish;
 
 
 	public string[] def_wordLilPpl;
+	public string[] def_wordLilPplTranslation;
 	public Sprite [] def_wordLilPplPhotos;
 	public string[] def_BodyLilPplEnglish;
 	// Use this for initialization
@@ -27,20 +34,39 @@ public class DefinitionRenderer : MonoBehaviour {
 
 	public void SetUpText (Text Definition) 
 	{
-		Debug.Log (Definition.text);
-		for (int i = 0; i < def_wordSas.Length; i++) 
-		{
-			//Debug.Log (Definition.text.ToLower()+"///"+def_wordSas[i].ToLower());
 
-			if (Definition.text.ToLower().Equals(def_wordSas[i].ToLower())) {
-			//if(String.Compare(Definition.text.ToLower() , def_wordSas[i].ToLower()) == 1){
-			//if ( Definition.text.ToLower() == def_wordSas[i].ToLower()) {
-				GetComponentInChildren<Text> ().text = def_BodySasEnglish [i];
-				RenderPlacement.sprite = def_wordSasPhotos [i];
+		Debug.Log (DataManager.currentStoryName);
+		if (DataManager.currentStoryName == "sasquatch") {
+			//Debug.Log (Definition.text);
+			for (int i = 0; i < def_wordSas.Length; i++) {
+				//Debug.Log (Definition.text.ToLower()+"///"+def_wordSas[i].ToLower());
 
-				Debug.Log (Definition.text.ToLower()+"///"+def_wordSas [i]);
-			} 
+				if (Definition.text.ToLower ().Equals (def_wordSas [i].ToLower ())) {
+
+					title.text = def_wordSas [i];
+					TextTranslation.text = def_wordSasTranslation [i];
+					RenderPlacement.sprite = def_wordSasPhotos [i];
+					TextBody.text = def_BodySasEnglish [i];
+					//Debug.Log (Definition.text.ToLower()+"///"+def_wordSas [i]);
+				} 
+			}
+		} else {
+
+			for (int i = 0; i < def_wordLilPpl.Length; i++) {
+				//Debug.Log (Definition.text.ToLower()+"///"+def_wordSas[i].ToLower());
+
+				if (Definition.text.ToLower ().Equals (def_wordLilPpl [i].ToLower ())) {
+
+					title.text = def_wordLilPpl [i];
+					TextTranslation.text = def_wordLilPplTranslation [i];
+					RenderPlacement.sprite = def_wordLilPplPhotos [i];
+					TextBody.text = def_BodyLilPplEnglish [i];
+					//Debug.Log (Definition.text.ToLower()+"///"+def_wordSas [i]);
+				} 
+			}
+
 		}
+
 	}
 
 }
