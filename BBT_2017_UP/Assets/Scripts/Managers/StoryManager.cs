d6362f9727fc06c01c1025a5730478945a22557d
Maurice Grela <maurice.grela@gmail.com>
2018-04-19 17:12:30 -0700

    Icons have been implemented.

--- BBT_2017_UP/Assets/Scripts/Managers/StoryManager.cs
+++ BBT_2017_UP/Assets/Scripts/Managers/StoryManager.cs
@@ -18,49 +18,48 @@ public class StoryManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		//Set references 
 		PageManager = GameObject.FindGameObjectWithTag ("PageManager");
 		Canvas = GameObject.FindGameObjectWithTag ("Canvas");
 
-		if (isLoadingLevel == true) {
+		if (isLoadingLevel == true) {//If this is going to load a different streaming package, load it here. 
+			
 			PageManager.GetComponent<PageManager>().audioIndex = 0;
 			DataManager.LoadStory (DataManager.currentStoryName, StreamingAssetsCounter.ToString());
 		}
 			else if (StreamingAssetsCounter.ToString() != DataManager.CurrentAssetPackage.ToString()) 
-			{
+			{//this condition will trigger when the player loads a level from the menu that requires a streaming asset that is not currently loaded.
 			DataManager.LoadStory (DataManager.currentStoryName, StreamingAssetsCounter.ToString());
 			}
 
-		//Debug.Log (StreamingAssetsCounter.ToString() +"/"+ DataManager.CurrentAssetPackage.ToString());
-
+		//This variable loads the offset from the page manager so that the level starts off at the right passage.
 		int chapterOffset = PageManager.GetComponent<PageManager> ().ChapterOffSet;
 
 		for(int i = 0; i < Canvas.transform.GetChildCount(); i++)
-		{
+		{//This loop generates the book mark dots 
 			if (Canvas.transform.GetChild (i).name == "UI Dots") {
 				Canvas.transform.GetChild (i).GetComponent<DotGenerator> ().GenrateTheDots (pagesPerScene);
 			}
 		}
 
 		if (PageManager.GetComponent<PageManager> ().isGoingBack == false) {
-			//Debug.Log ("isGoingBack="+PageManager.GetComponent<PageManager> ().isGoingBack);
+			//Set up the narrative variables
 			PageManager.GetComponent<PageManager> ().AssetAssigner (LevelName, AudioIndexPosition+chapterOffset);
 			PageManager.GetComponent<PageManager> ().GoToPage (AudioIndexPosition+chapterOffset);
 			PageManager.GetComponent<PageManager> ().ChapterskipSetCharacters(chapterOffset);
-		} else {
-			//Debug.Log ("isGoingBack="+PageManager.GetComponent<PageManager> ().isGoingBack);
-			PageManager = GameObject.FindGameObjectWithTag ("PageManager");
-			PageManager.GetComponent<PageManager> ().AssetAssigner (LevelName,pagesPerScene);
-			PageManager.GetComponent<PageManager> ().GoToPage (AudioIndexPosition+(pagesPerScene-1));	
-			PageManager.GetComponent<PageManager> ().isGoingBack = false;
-		}
+			} 
+				else 
+				{//If the player is going backwards
+				//Debug.Log ("isGoingBack="+PageManager.GetComponent<PageManager> ().isGoingBack);
+				//PageManager = GameObject.FindGameObjectWithTag ("PageManager");
+				PageManager.GetComponent<PageManager> ().AssetAssigner (LevelName,pagesPerScene);
+				PageManager.GetComponent<PageManager> ().GoToPage (AudioIndexPosition+(pagesPerScene-1));	
+				PageManager.GetComponent<PageManager> ().isGoingBack = false;
+				}
 	PageManager.GetComponent<PageManager> ().ChapterOffSet = 0;
 	}
 
-	public void StartFromEndOfLevel () {
-
-	}
-
-	
 	// Update is called once per frame
 	void Update () {
 		
