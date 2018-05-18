a90ebfc123e8818dc6864575318e073dc625bb1e
Maurice Grela <maurice.grela@gmail.com>
2018-05-18 14:36:58 -0700

    Committing successful test of videos.

---
+++ BBT_2017_UP/Assets/Scripts/MainMenu/VideoManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Video;
+using UnityEngine;
+
+public class VideoManager : MonoBehaviour {
+
+
+	public VideoClip[] videoClips;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeSource()
+	{
+		
+		//GetComponent<VideoPlayer>().Source = videoClips[3];
+	}
+}
